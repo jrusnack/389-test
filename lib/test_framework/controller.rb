5cb27c9110e18795708bf23f76b3135ca88ea6ea
Jan Rusnacko <jrusnack@redhat.com>
2013-06-05 17:43:23 +0200

    Creating output directory if nonexistent.

--- lib/test_framework/controller.rb
+++ lib/test_framework/controller.rb
@@ -15,28 +15,28 @@ class Controller
 		@testsuites << Testsuite::Builder.get_testsuite
 	end
 
-	def execute(output_dir)
+	def execute(output_dir=nil)
 		@testsuites.each do |testsuite|
 			output_file = output_dir ? output_dir + "/#{testsuite.name}" : nil
 			testsuite.execute(output_file)
 		end
 	end
 
-	def write_xml_report(output_dir)
+	def write_xml_report(output_file)
 		@xml_report = REXML::Document.new
 		@xml_report << REXML::XMLDecl.new
 		@testsuites.each do |testsuite|
 			@xml_report.add(testsuite.to_xml)
 		end
-		File.open(output_dir + "/results.xml", 'w') {|file| @xml_report.write(file, 4)}
+		File.open(output_file, 'w') {|file| @xml_report.write(file, 4)}
 	end
 
-	def write_junit_report(output_dir)
+	def write_junit_report(output_file)
 		@junit_report = REXML::Document.new
 		@junit_report << REXML::XMLDecl.new
 		@testsuites.each do |testsuite|
 			@junit_report.add(testsuite.to_junit_xml)
 		end
-		File.open(output_dir + "/junit.xml", 'w') {|file| @junit_report.write(file, 4)}
+		File.open(output_file, 'w') {|file| @junit_report.write(file, 4)}
 	end
 end
