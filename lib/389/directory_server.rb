27e18edbef61931570a84b814d9bc6bf7b31af00
Jan Rusnacko <jrusnack@redhat.com>
2013-07-23 10:21:23 +0200

    Splitting DirectoryServer class into multiple files.
    Files will group methods with regard to functionality (e.g. replication
    related methods..). This will prevent having one huge file with DS
    implementation.

--- lib/389/directory_server.rb
+++ lib/389/directory_server.rb
@@ -1,154 +1,27 @@
 
-
 require 'ldap/ldapserver'
 require 'ldap/ldap'
 require 'util/os'
 require 'util/log'
 
+# Implementation of DirectoryServer class is split into several files.
+# This allows to group methods related to certain functionality of DS (like
+# SLL, replication, DNA, ...) in one file and prevents having one huge file with
+# DS implementation.
+require '389/basic_functionality'
+
 class DirectoryServer < LdapServer
+    # include LogMixin to add log() and log_error() methods to DirectoryServer
+    # both use @log instance variable, so make sure it is defined for DS instance
     include LogMixin
+
+    # include LDAP helper functions
     include Ldap
+
     # include OS will mix-in methods from OS module as instance methods
     # extend OS will mix-in methods from OS module as class methods
-    # awful, but necessary if we want to use OS methods in both 
+    # awful, but necessary if we want to use OS methods in both
     # instance (like self.get_instance) and class methods (like initialize)
     include OS
     extend OS
-
-    def self.get_instance(log, params={})
-        port = get_free_port
-        1000.times do |i|
-            name = get_hostname + "#{i}"
-            if ! self.instance_exists?(name)
-                params[:port] = port
-                params[:name] = name
-                return self.new(log, params)
-            end
-        end
-        raise RuntimeError.new("Could not create new instance of Directory Server. No free instance name.")
-    end
-
-    def self.instance_exists?(name)
-        File.exists? "/etc/dirsrv/slapd-#{name}"
-    end
-
-    def initialize(log, params={})
-        @log = log
-        
-        # Set general options
-        @fqdn    = params[:fqdn]     || get_fqdn
-        @user    = params[:user]     || "nobody"
-        @group   = params[:group]    || "nobody"
-
-        # Set instance options
-        @default_backend    = params[:backend]  || "userRoot"
-        @default_suffix     = params[:suffix]   || "dc=example,dc=com"
-        @name               = params[:name]     || get_hostname
-        @host               = params[:host]     || "localhost"
-        @port               = params[:port]     || 389
-        @root_dn            = params[:root_dn]  || "cn=directory manager"
-        @root_pw            = params[:root_pw]  || "Secret123"
-
-        super(@host, @port, @root_dn, @root_pw)
-
-        # Set root directory of instance, used later for starting/stopping the instance
-        # $platform_64 bit global variable is set in Ldapclients module
-        if is_64? then
-            @iroot = "/usr/lib64/dirsrv/slapd-#{@name}"
-        else
-            @iroot = "/usr/lib/dirsrv/slapd-#{@name}"
-        end
-    end
-
-    def setup
-        # Create config for new DS instance
-        config = <<-EOF
-        [General]
-        FullMachineName=#{@fqdn}
-        SuiteSpotUserID=#{@user}
-        SuiteSpotGroup=#{@group}
-
-        [slapd]
-        ServerPort=#{@port}
-        ServerIdentifier=#{@name}
-        Suffix=#{@default_suffix}
-        RootDN=#{@root_dn}
-        RootDNPwd=#{@root_pw}
-        ds_bename=#{@default_backend}
-        EOF
-
-        # Remove spaces from the beginning and the end of lines
-        config = config.lines.to_a.map{|line| line.strip}.join("\n")
-        config_file = get_tmp_file
-        File.open(config_file, "w+") {|file| file.write(config)}
-
-        sh "sudo setup-ds.pl -s -f #{config_file}"
-        
-        if ! $?.success? then
-            raise RuntimeError.new("Failed to create new instance. Return code: #{$?.exitstatus}")
-        end
-    end
-
-    # Executes remove-ds.pl script on instance.
-    def remove
-        sh "sudo remove-ds.pl -i slapd-#{@name}"
-        if ! $?.success?
-            raise RuntimeError.new("Error occurred while removing instance. Return code: #{$?.exitstatus}")
-        end
-    end
-
-    def restart
-        sh "sudo #{@iroot}/restart-slapd"
-        if ! $?.success? then
-            raise RuntimeError.new("Error occurred while restarting instance. Return code: #{$?.exitstatus}")
-        end
-    end
-
-    def start
-        # Don`t start if server is already running
-        return if running?
-
-        sh "sudo #{@iroot}/start-slapd"
-        if ! $?.success? then
-            raise RuntimeError.new("Error occurred while starting instance. Return code: #{$?.exitstatus}")
-        end
-    end
-
-    def stop
-        # Don`t stop if server is already stopped
-        return if !running?
-
-        sh "sudo #{@iroot}/stop-slapd"
-        if ! $?.success? then
-            raise RuntimeError.new("Error occurred while stopping instance. Return code: #{$?.exitstatus}")
-        end
-    end
-
-    def running?
-        if `sudo service dirsrv status #{@name}`.include?("is running.") then
-            return true
-        else
-            return false
-        end
-    end
-
-    def add_user(dn)
-        log "Adding user #{dn}"
-        rdn = get_rdn(dn)
-        input = <<-EOF
-            dn: #{dn}
-            objectClass: top
-            objectClass: person
-            objectClass: inetOrgPerson
-            cn: #{rdn}
-            sn: #{rdn}
-            uid: #{rdn}
-        EOF
-        log self.ldapadd_r(input)
-
-        if ! $?.success? then
-            raise RuntimeError.new("Error occurred while adding new user. Return code: #{$?.exitstatus}")
-        end
-    end
-
 end
