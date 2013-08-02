de2b92bdc591d2ac91511cf16c5f10a1dcdd705f
Jan Rusnacko <jrusnack@redhat.com>
2013-08-02 21:47:45 +0200

    Adding replication wrappers and modifying testsuite.

--- lib/389/replication.rb
+++ lib/389/replication.rb
@@ -4,6 +4,7 @@ class DirectoryServer < LdapServer
     attr_reader :replication_manager_dn, :replication_manager_pw
 
     def add_replication_manager(dn="cn=replication manager,cn=config", password="Secret123")
+        log "Adding replication manager #{dn}"
         @replication_manager_dn = dn
         @replication_manager_pw = password
         input = <<-EOF
@@ -17,15 +18,15 @@ class DirectoryServer < LdapServer
             passwordExpirationTime: 20380119031407Z
             nsIdleTimeout: 0
         EOF
-        self.ldapmodify_r(input)
+        log self.ldapadd_r(input)
         if ! $?.success? then
-            raise RuntimeError.new("Failed to add replication manager #{dn} \
-                with password #{password}. Return code: #{$?.exitstatus}")
+            raise RuntimeError.new("Failed to add replication manager \"#{dn}\" with password #{password}. Return code: #{$?.exitstatus}")
         end
     end
 
     def enable_changelog(dir="/var/lib/dirsrv/slapd-#{@name}/changelogdb")
-        input = <<-EOF
+        log "Enabling changelog: #{dir}"
+        log self.ldapadd_r <<-EOF
             dn: cn=changelog5,cn=config
             objectclass: top
             objectclass: extensibleObject
@@ -33,9 +34,51 @@ class DirectoryServer < LdapServer
             nsslapd-changelogdir: #{dir}
             nsslapd-changelogmaxage: 10d
         EOF
-        self.ldapadd_r(input)
         if ! $?.success? then
             raise RuntimeError.new("Failed to add changelog \'#{dir}\'. Return code: #{$?.exitstatus}")
         end
+    end
+
+    def enable_supplier(suffix, id)
+        log "Enabling supplier: suffix #{suffix} with id #{id}"
+        enable_replica(suffix, id, 3, 1)
+        if ! $?.success? then
+            raise RuntimeError.new("Failed to enable supplier: suffix #{suffix} with id #{id}. Return code: #{$?.exitstatus}")
+        end
+    end
+
+    def enable_consumer(suffix, id)
+        log "Enabling consumer: suffix #{suffix} with id #{id}"
+        enable_replica(suffix, id, 2, 0)
+        if ! $?.success? then
+            raise RuntimeError.new("Failed to enable consumer: suffix #{suffix} with id #{id}. Return code: #{$?.exitstatus}")
+        end
+    end
+
+    def enable_hub(suffix, id)
+        log "Enabling hub: suffix #{suffix} with id #{id}"
+        enable_replica(suffix, id, 2, 1)
+        if ! $?.success? then
+            raise RuntimeError.new("Failed to enable hub: suffix #{suffix} with id #{id}. Return code: #{$?.exitstatus}")
+        end
+    end
+
+    private
+
+    def enable_replica(suffix, id, type, flags)
+        log self.ldapadd_r <<-EOF
+            dn: cn=replica,cn="#{suffix}",cn=mapping tree,cn=config
+            changetype: add
+            objectclass: top
+            objectclass: nsds5replica
+            objectclass: extensibleObject
+            cn: replica
+            nsds5replicaroot: #{suffix}
+            nsds5replicaid: #{id}
+            nsds5replicatype: #{type}
+            nsds5flags: #{flags}
+            nsds5ReplicaPurgeDelay: 604800
+            nsds5ReplicaBindDN: #{@replication_manager_dn}
+        EOF
     end
 end
--- test/replication/replication.rb
+++ test/replication/replication.rb
@@ -8,25 +8,50 @@ testsuite "replication"
 
 	startup do
 		@master1 = DirectoryServer.get_instance(@log)
-		@master2 = DirectoryServer.get_instance(@log)
+		@master1.add_replication_manager
+		@master1.enable_changelog
+		@master1.enable_supplier('dc=example,dc=com', 1)
 	end
 
-	testcase "tc01"
-		purpose "Add replication managers"
-		run do
-			@master1.add_replication_manager
-			@master2.add_replication_manager
-		end
+	testcase "bug974719"
+		purpose "rhds90 crash on tombstone modrdn"
+		# Specify data that will be fed to the testcase
+		with 'tuser1', nil, 				1, 53
+		with 'tuser2', nil, 				0, 53
+		with 'tuser3', 'dc=example,dc=com', 1, 53
+		with 'tuser4', 'dc=example,dc=com', 0, 53
+		run do |user, new_superior, deleteoldrdn, expected_rc|
+			# Add user
+			log @master1.add_user("uid=#{user},ou=people,dc=example,dc=com")
 
-	testcase "tc02"
-		purpose "Enable changelogs"
-		run do
-			@master1.enable_changelog
-			@master2.enable_changelog
+			# Delete him to create tombstone entry
+			log @master1.ldapdelete_r("uid=#{user},ou=people,dc=example,dc=com")
+
+			# Get the nsuniqueid of the tombstone
+			nsuniqueid = @master1.ldapsearch_r(:base => "ou=people,dc=example,dc=com", \
+				:filter => "(&(objectclass=nstombstone)(uid=#{user}))", :attributes => 'nsuniqueid').get_attr_value('nsuniqueid')
+			log "nsuniqueid of tombstone is #{nsuniqueid}"
+
+			# Create the input for ldapmodify
+			input = <<-EOF
+				dn: nsuniqueid=#{nsuniqueid}, uid=#{user},ou=people,dc=example,dc=com
+				changetype: modrdn
+				newrdn: nsuniqueid=#{nsuniqueid}
+				deleteoldrdn: #{deleteoldrdn}
+			EOF
+			# If new_superior is specified (not nil), add it to the input
+			if new_superior then
+				input << "newSuperior: #{new_superior}"
+			end
+
+			# Try to modrdn and log the output
+			log @master1.ldapmodify_r(input)
+
+			# Verify that returned return code is the same as expected return code
+			assert_equal("Modrdn on tombstone should be refused with unwilling to perform.", expected_rc, $?.exitstatus)
 		end
 
 	cleanup do
-		@master1.remove if @master1
-		@master2.remove if @master2
+		@master1.remove
 	end
 end
