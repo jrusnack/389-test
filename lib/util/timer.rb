2359110680e12226014b69312a84a87bb8348936
Jan Rusnacko <jrusnack@redhat.com>
2013-08-06 22:47:09 +0200

    Adding licensing information.
    Fixes #11

--- lib/util/timer.rb
+++ lib/util/timer.rb
@@ -1,3 +1,26 @@
+# 389-test - testing framework for 389 Directory Server
+#
+# Copyright (C) 2013 Jan Rusnacko
+#
+# This file is part of 389-test.
+#
+# 389-test is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+#
+# 389-test is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with 389-test. If not, see <http://www.gnu.org/licenses/>.
+#
+# For alternative license options, contact the copyright holder.
+#
+# Jan Rusnacko <rusnackoj@gmail.com>
+
 class Timer
   
     def start
